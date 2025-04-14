14d036841e0459d86ea44a9dd63b52d1eb1f6522
NaderElGhamryQA <naderbailey97@gmail.com>
2025-04-14 17:06:18 +0200

    Delete src directory

--- src/main/java/API/HelperClasses/API_Endpoints.java
+++
@@ -1,7 +0,0 @@
-package API.HelperClasses;
-
-public class API_Endpoints {
-    public static final String createUserEndpoint = "/api/users";
-    public static final String updateUserEndpoint = "/api/users/id";
-    public static final String getUserEndpoint = "/api/users/id";
-}
